65ee7a56ab84679bc4a50e23240b8f253be4c42f
whaas <wrhaas@gmail.com>
2025-04-21 16:37:02 -0600

    Initial commit

--- resources/USGSdecollar.R
+++ resources/USGSdecollar.R
@@ -1,23 +1,23 @@
 #Decollars USGS Quad maps from https://ngmdb.usgs.gov/topoview/
-#by Randy Haas 14 April 2025
+#by Randy Haas 14 April 2025. revised 21 April 2025
 #requires the tif and xml files from the USGS download site, https://ngmdb.usgs.gov/topoview/
 
 require("terra")
 require("XML")
 
-decollar<-function(file){
+decollar<-function(dir,file){
     #file=file name to be decollared. Should include "tif" extension.
-    r<-rast(file)
-    xml<-xmlToList(xmlParse(paste0(file,".xml")))
-    w<-as.numeric(xml$idinfo$spdom$bounding$westbc)
+    #dir=working directory where geotif and associated xml files are located
+    r<-rast(paste0(dir,file))
+    xml<-xmlToList(xmlParse(paste0(dir,file,".xml"))) #get metadata from xml file
+    w<-as.numeric(xml$idinfo$spdom$bounding$westbc) #get bounding coorindates from xml
     e<-as.numeric(xml$idinfo$spdom$bounding$eastbc)
     n<-as.numeric(xml$idinfo$spdom$bounding$northbc)
     s<-as.numeric(xml$idinfo$spdom$bounding$southbc)
-    long<-(w+e)/2
-    r2<-project(r,"EPSG:4269") #project to NAD83 geographic
-    r3<-crop(r2,ext(w,e,s,n)) #crop to lat/long extents specified in xml file.
-    r4<-project(r3,crs(r)) #project back to original crs.
-    writeRaster(r4,paste0(gsub(".tif","",file),"_decollar.tif"))
+    ext1<-vect(ext(w,e,s,n),crs="EPSG:4269") #create a polygon from bounding coordinates
+    ext2<-project(ext1,crs(r))
+    r2<-crop(mask(r,ext2),ext2) #crop to lat/long extents specified in xml file.
+    writeRaster(r2,paste0(dir,gsub(".tif","",file),"_decollar.tif"),overwrite=T)
     print("Complete. Find the decollared file in the working directory.") 
 }
 
